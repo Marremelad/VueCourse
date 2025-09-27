c2f1b32db4a72100c41878e912d43422cb2ff216
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-09-27 12:16:12 +0200

    basics 3, events in action

--- Basics/basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ Basics/basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,8 +2,25 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    add() {
+      this.counter++;
+    },
+    reduce() {
+      this.counter > 0 ? this.counter-- : 0;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    setConfirmedName() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {},
   },
 });
 
-app.mount('#events');
+app.mount("#events");
